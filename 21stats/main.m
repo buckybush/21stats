0a08c85d901a60d344c80b17d333ee9e24c02611
Bucky <buckybush@gmail.com>
2011-07-26 13:29:39 -0400

    first commit

---
+++ 21stats/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  21stats
+//
+//  Created by Bucky Bush on 7/26/11.
+//  Copyright 2011 Northwest GA RESA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
